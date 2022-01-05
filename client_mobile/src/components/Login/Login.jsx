cb236711d6916310ebbc27b2c3f294205b256be9
Franco Pizzi <fran.pizzi@hotmail.com>
2022-01-05 11:32:40 -0300

    login alert

--- client_mobile/src/components/Login/Login.jsx
+++ client_mobile/src/components/Login/Login.jsx
@@ -1,20 +1,47 @@
-import * as React from 'react';
-import { TouchableOpacity , StyleSheet, Text, TextInput ,View } from 'react-native';
+import React, { useState } from 'react';
+import { TouchableOpacity , StyleSheet, Text, TextInput ,View , Image} from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 export default function Login() {
   const navigation = useNavigation();  
+  const [inputs, setInput] = useState({
+    user: '',
+    password: '',
+  });
+  const users = [{user: 'franco' , password: '1234'},{user: 'juani' , password: '1234'}]
+  function onPressBtn() {
+    console.log(inputs)
+    const user = users.find((element) => element.user === inputs.user && element.password === inputs.password )
+    if (user) {
+      alert ("Usuario valido")
+    }
+    else {
+      alert ("Usuario no valido")
+    }
+  }
+
   return (
       <View>
         <View style={styles.inputContainers}>
+        <Image source={require('./Logo.jpg')} />
           <TextInput
           placeholder="Usuario"
           style={styles.input}
+          onChangeText={ user => setInput({...inputs , user})}
+          defaultValue={inputs.user}
           />
           <TextInput
           placeholder="Contraseña"
           style={styles.input}
+          onChangeText={ password => setInput({...inputs , password})}
+          defaultValue={inputs.password}
+          secureTextEntry={true}
           />
+          <TouchableOpacity onPress={onPressBtn}>
+            <View style={styles.button}>
+              <Text style={styles.buttonText}>Iniciar sesión</Text>
+            </View>
+          </TouchableOpacity>
         </View>
         <View style={styles.registerContainer}>
           <Text style={styles.acount}>¿No tienes una cuenta?</Text>
@@ -32,19 +59,32 @@ const styles = StyleSheet.create({
     height: 40,
 
     marginTop:15,
+    
+    borderRadius: 20,
+    borderWidth: 1,
 
-    borderLeftWidth: 1,
-    borderTopWidth: 1,
-    borderRightWidth: 1,
-    borderBottomWidth: 1,
+    backgroundColor: 'white',
 
-    borderTopLeftRadius: 20,
-    borderTopRightRadius: 20,
-    borderBottomRightRadius: 20,
-    borderBottomLeftRadius: 20,
+    paddingLeft: 10,
   },
   inputContainers: {
     alignItems: 'center',
+  },
+  button: {
+    marginTop: 30,
+    width: 130,
+    height: 35,
+    alignItems: 'center',
+    backgroundColor: 'white',
+    borderRadius: 20,
+    borderWidth: 1,
+    borderColor: 'black',
+    justifyContent: 'center'
+  },
+  buttonText: {
+    textAlign: 'center',
+    padding: 20,
+    color: 'black'
   },
   registerContainer: {
     display: 'flex',
