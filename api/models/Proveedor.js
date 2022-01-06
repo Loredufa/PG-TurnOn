ac7cf02e219d75480f0f7f9c154ab06a523a9cb7
Lorena <88742175+Loredufa@users.noreply.github.com>
2022-01-06 12:22:50 -0300

    comienzo de los modelos

---
+++ api/models/Proveedor.js
@@ -0,0 +1,34 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('user', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    id: {
+      type:DataTypes.UUID,
+      defaultValue:DataTypes.UUIDV4,
+      allowNull:false,
+      primaryKey:true,
+    },   
+    lastname: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    phone: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    mail: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
+    })
+  }
