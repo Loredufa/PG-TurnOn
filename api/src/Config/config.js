ad58fc362065c30efdc1a8eddbc84766d3a61334
Leo Wirth <leo_wirth@hotmail.com>
2022-01-06 15:46:11 -0300

    Se agregan modelos a db.js

--- api/src/Config/config.js
+++ api/src/Config/config.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 
 module.export = {
   dbUser: process.env.DB_USER || "postgres",
+  dbName: process.env.DB_NAME || "turnon",
   debPort: process.env.DB_PORT || "5432",
   dbHost: process.env.DB_HOST || "localhost",
   dbPassword: process.env.DB_PASSWORD || "admin",
--- api/src/Config/db.js
+++ api/src/Config/db.js
@@ -1,6 +1,10 @@
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const { DB_USER, DB_HOST, DB_PASSWORD } = require("./config");
+const UserModel = require("../models/User");
+const ProveedorModel = require("../models/Proveedor");
+const AvailableModel = require("../models/Available");
+const FieldModel = require("../models/Field");
 
 const sequelize = new Sequelize(
   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/food`,
@@ -9,3 +13,20 @@ const sequelize = new Sequelize(
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
   }
 );
+
+const User = UserModel(sequelize);
+const Proveedor = ProveedorModel(sequelize);
+const Available = AvailableModel(sequelize);
+const Field = FieldModel(sequelize);
+
+// --------------------------- Relaciones ---------------------------------//
+// Recipe.belongsToMany(Diet, { through: "intermediateTable" }); // Relacion de muchos a muchos con la taabla intermedia "intermediateTable"
+// Diet.belongsToMany(Recipe, { through: "intermediateTable" }); // Relacion de muchos a muchos con la taabla intermedia "intermediateTable"
+
+module.exports = {
+  conn: sequelize, // para importart la conexión { conn } = require('./db.js');
+  User,
+  Proveedor,
+  Available,
+  Field,
+};
