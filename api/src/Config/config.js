aeb40947deb5406a3517f1592ec6b90bb3874868
unknown <lubelleggia@gmail.com>
2022-01-06 19:28:50 -0300

    se resolvio error mencionado por Leo

--- api/src/Config/config.js
+++ api/src/Config/config.js
@@ -7,7 +7,7 @@ var resp = {
   dbName: process.env.DB_NAME || "turnon", //DB_NAME || "turnon", // process.env.DB_NAME || "turnon",
   dbPort: process.env.DB_PORT || "5432", //DB_PORT || "5432", // process.env.DB_PORT || "5432",
   dbHost: process.env.DB_HOST || "localhost", //DB_HOST || "localhost", // process.env.DB_HOST || "localhost",
-  dbPassword: process.env.DB_PASSWORD || "admin", //DB_PASSWORD || "admin", //process.env.DB_PASSWORD || "admin",
+  dbPassword: process.env.DB_PASSWORD || "12345", //DB_PASSWORD || "admin", //process.env.DB_PASSWORD || "admin",
 };
 // console.log(resp);
 module.exports = resp;
