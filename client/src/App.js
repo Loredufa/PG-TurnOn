f85c9b07d0305fb471cdf7e463e8854f60b778ae
Orhianna <orhianna.ss@gmail.com>
2022-01-09 15:25:16 -0300

    Agregamos NavBar

--- client/src/App.js
+++ client/src/App.js
@@ -5,11 +5,13 @@ import Home from "./Components/Home";
 import Login from "./Components/Login";
 import React from "react";
 import Registro from "./Components/Registro";
+import Navbar from "./Components/Navbar"
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        <Navbar/>
         <Switch>
           <Route exact path="/registro" component={Registro} />
           <Route exact path="/login" component={Login} />
