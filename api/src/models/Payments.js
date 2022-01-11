204134176f928a060052a234a78cbf257cfad5a1
Leonel Wirth <41310397+LeonelWirth@users.noreply.github.com>
2022-01-11 10:10:24 -0300

    Se corrige idCourt

--- api/src/models/Payments.js
+++ api/src/models/Payments.js
@@ -9,7 +9,7 @@ module.exports = (sequelize) => {
       type: DataTypes.INTEGER,
       allowNull: false,
     },
-    idCort: {
+    idCourt: {
       type: DataTypes.STRING,
       allowNull: false,
     },
