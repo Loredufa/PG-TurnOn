bbbc01d509efe0ef9089d4c36133cb74e7957e16
Leo Wirth <leo_wirth@hotmail.com>
2022-01-10 23:42:19 -0300

    Se agrega delete de pagos

--- api/src/routes/supplier/payments.js
+++ api/src/routes/supplier/payments.js
@@ -3,9 +3,11 @@ const router = Router();
 const { createPayment } = require("./controllers/createPayment");
 const { updatePayment } = require("./controllers/updatePayment");
 const { getPayments } = require("./controllers/getPayment");
+const { deletePayments } = require("./controllers/deletePayment");
 
 router.post("/", createPayment);
 router.put("/:id", updatePayment);
 router.get("/", getPayments);
+router.delete("/:id", deletePayments);
 
 module.exports = router;
