4038fa52adc5472a3cbbdc916235b09024b7e3a4
Juan Huarte <juan_huarte@hotmail.com>
2022-01-11 23:32:46 -0300

    Agrego cambios del componente Register

--- client_mobile/src/components/Register/validationFunc.js
+++ client_mobile/src/components/Register/validationFunc.js
@@ -1,15 +1,20 @@
 export function validationFunc(input) {
   let errors = {};
-  if (input.name.length === 0) errors.name = "SE REQUIERE NOMBRE!!";
-  if (input.lastname.length === 0) errors.lastname = "SE REQUIERE APELLIDO!!";
-  if (input.phone.length < 10) errors.phone = "NUMERO ERRONEO !!";
-  if (input.email.length === 0) errors.email = "SE REQUIERE EMIAL!!";
-  else if (!/\S+@\S+\.\S+/.test(input.email)) errors.email = "EMAIL INVALIDO!!";
-  if (input.password.length === 0) errors.password = "SE REQUIERE CONTRASEÑA!!";
+  if (input.name.length === 0) errors.name = "Se requiere Nombre";
+  if (input.lastname.length === 0) errors.lastname = "Se requiere Apellido";
+  if (input.phone.length < 10) errors.phone = "Se requiere numero de Telefono";
+  if (input.email.length === 0) errors.email = "Se requiere un Mail";
+  else if (!/\S+@\S+\.\S+/.test(input.email)) errors.email = "Mail invalido";
+  if (input.password.length === 0) errors.password = "Se requiere Contraseña";
   else if (!/^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/.test(input.password))
-    errors.password = "CONTRASEÑA INVALIDA!!";
+    errors.password =
+      "La contraseña debe tener entre 8 y 16 caracteres, al menos un dígito, al menos una minúscula y al menos una mayúscula. Puede tener otros símbolos";
   if (input.password !== input.repassword)
-    errors.repassword = "LA CONTRASEÑA NO COINCIDE";
+    errors.repassword = "La contraseña no coincide";
+  else if (
+    !/^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/.test(input.repassword)
+  )
+    errors.repassword = "Se requiere Contraseña";
   return errors;
 }
 
