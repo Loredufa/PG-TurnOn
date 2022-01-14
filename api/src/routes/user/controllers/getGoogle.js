7f24b1358e83ab0ffb51feee8177cc9ecb09f2d2
Leo Wirth <leo_wirth@hotmail.com>
2022-01-14 15:48:34 -0300

    Se agrega created a la respuesta

--- api/src/routes/user/controllers/getGoogle.js
+++ api/src/routes/user/controllers/getGoogle.js
@@ -1,53 +1,47 @@
-const { User } = require("../../../db")
-const jwt = require("jsonwebtoken")
-const bcrypt = require("bcrypt")
-const jwtDecode = require('jwt-decode')
-
+const { User } = require("../../../db");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const jwtDecode = require("jwt-decode");
 
 const getGoogle = async (req, res) => {
-    
-    const {token} = req.query;
-    const decoded = jwtDecode(token);
-    const { given_name , family_name, email} = decoded;
-    //res.json ({given_name ,family_name, email});
-    const mail = email;
-    const name = given_name;
-    const lastname = family_name;
-    const password = bcrypt.hashSync('google', 10)
-    const phone = '0000000000';
+  const { token } = req.query;
+  const decoded = jwtDecode(token);
+  const { given_name, family_name, email } = decoded;
+  //res.json ({given_name ,family_name, email});
+  const mail = email;
+  const name = given_name;
+  const lastname = family_name;
+  const password = bcrypt.hashSync("google", 10);
+  const phone = "0000000000";
 
-    if (mail) {
-        try {
-        const [user, created] = await User.findOrCreate({
-            where: { mail: mail },
-            defaults: {
-                name,
-                lastname,
-                password,
-                phone
-            }
-        });
-        const jwtToken = jwt.sign(
-            { id: user.id, email: user.mail, }, 
-            "secreto#$%123" /* <- esto puede ser cualquier cosa, pero idealmente tiene que estar guardado en el .env */
-            )
-            
-            if (created) {
-                res.json({ user: user , token: jwtToken })
-            }
-            else {
-                const user = {mail , name, lastname, password, phone}
-                res.json({ user: user , token: jwtToken })
-            }
-        }
-        catch (error) {
-        throw new Error(error);
-        }
-    } else {
-        return res.json({ message: "Datos incorrectos"})
+  if (mail) {
+    try {
+      const [user, created] = await User.findOrCreate({
+        where: { mail: mail },
+        defaults: {
+          name,
+          lastname,
+          password,
+          phone,
+        },
+      });
+      const jwtToken = jwt.sign(
+        { id: user.id, email: user.mail },
+        "secreto#$%123" /* <- esto puede ser cualquier cosa, pero idealmente tiene que estar guardado en el .env */
+      );
+
+      if (created) {
+        res.json({ user: user, token: jwtToken, created });
+      } else {
+        const user = { mail, name, lastname, password, phone };
+        res.json({ user: user, token: jwtToken, created });
+      }
+    } catch (error) {
+      throw new Error(error);
     }
-    
-            
-}
+  } else {
+    return res.json({ message: "Datos incorrectos" });
+  }
+};
 
-module.exports = { getGoogle }
+module.exports = { getGoogle };
