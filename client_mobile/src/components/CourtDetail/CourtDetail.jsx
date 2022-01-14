255a307794b8c896e5df78715d7012477c17c864
Juan Huarte <juan_huarte@hotmail.com>
2022-01-14 18:03:30 -0300

    Agrego el mapa
    Arregle algunos errores que teniamos para verlo con expo go y agregue el mapa

--- client_mobile/src/components/CourtDetail/CourtDetail.jsx
+++ client_mobile/src/components/CourtDetail/CourtDetail.jsx
@@ -10,20 +10,25 @@ import {
 import MaterialCommunityIcons from "react-native-vector-icons/Ionicons";
 import { useDispatch, useSelector } from "react-redux";
 import { addToFavorite, bookCourt } from "../../store/actions/index";
-import {styles} from './StyleCourtDetail';
+import { styles } from "./StyleCourtDetail";
 
 export default function CourtDetail({ route }) {
   const dispatch = useDispatch();
-  const favorites = useSelector(state => state.favorites);
-  console.log (favorites);
+  const favorites = useSelector((state) => state.favorites);
+  console.log(favorites);
   function handlerBooking(court) {
     dispatch(bookCourt(court));
   }
-  
+  const screenWidth = useSelector((state) => state.screenWidth);
+  const titleSize = useSelector((state) => state.titleSize);
+  const { court } = route.params;
+
+  //console.log(court);
+
   return (
     <View style={styles.container}>
       <View style={styles.nameContainer}>
-        <Text style={{ marginRight: 20 }}>{route.params.court.name}</Text>
+        <Text style={{ marginRight: 20 }}>{court.name}</Text>
         <MaterialCommunityIcons
           name="close-circle-outline"
           size={25}
@@ -32,11 +37,11 @@ export default function CourtDetail({ route }) {
         />
       </View>
       <Image
-        source={route.params.court.img}
+        source={court.img}
         style={{
           flex: 2,
-          height: route.params.dimension / 6,
-          width: route.params.dimension / 2,
+          height: screenWidth / 6,
+          width: screenWidth / 2,
           padding: 3,
           borderBottomLeftRadius: 10,
           borderTopLeftRadius: 10,
@@ -50,10 +55,10 @@ export default function CourtDetail({ route }) {
       </View>
       <Text style={{ flex: 1, borderWidth: 1 }}>Tamaño</Text>
       <Text style={{ flex: 1 }}>Precio: </Text>
-      <TouchableOpacity onPress={handlerBooking(route.params.court)}>
-        <View style={styles.button}>
-          <Text style={styles.buttonText}>Reservar</Text>
-        </View>
+      <TouchableOpacity style={styles.button} onPress={handlerBooking(court)}>
+        {/* <View style={styles.button}> */}
+        <Text style={styles.buttonText}>Reservar</Text>
+        {/* </View> */}
       </TouchableOpacity>
       <View
         style={{
@@ -63,32 +68,30 @@ export default function CourtDetail({ route }) {
           padding: 3,
         }}
       >
-        <TouchableOpacity
-          onPress={() => dispatch(addToFavorite(route.params.court))}
-        >
-          {favorites.find(element => element.name === route.params.court.name)?
-                        <MaterialCommunityIcons
-                        name="heart"
-                        size={25}
-                        color= 'red'
-                        style={{ marginLeft: 10, marginRight: 20 }}
-                        />:
-                        <MaterialCommunityIcons
-                        name="heart-outline"
-                        size={25}
-                        color= 'black'
-                        style={{ marginLeft: 10, marginRight: 20 }}
-                        />
-                      }
+        <TouchableOpacity onPress={() => dispatch(addToFavorite(court))}>
+          {favorites.find((element) => element.name === court.name) ? (
+            <MaterialCommunityIcons
+              name="heart"
+              size={25}
+              color="red"
+              style={{ marginLeft: 10, marginRight: 20 }}
+            />
+          ) : (
+            <MaterialCommunityIcons
+              name="heart-outline"
+              size={25}
+              color="black"
+              style={{ marginLeft: 10, marginRight: 20 }}
+            />
+          )}
         </TouchableOpacity>
         <MaterialCommunityIcons
           name="star"
           size={20}
           style={{ marginLeft: 20 }}
         />
-        <Text style={{ marginLeft: 5 }}>{route.params.court.rating}</Text>
+        <Text style={{ marginLeft: 5 }}>{court.rating}</Text>
       </View>
     </View>
   );
 }
-
--- client_mobile/src/components/FavoritesCourts/StyleFavoritesCourts.jsx
+++ client_mobile/src/components/FavoritesCourts/StyleFavoritesCourts.jsx
@@ -1,13 +1,11 @@
-import {
-    StyleSheet,
-  } from "react-native";
-  
-export 
-const styles = StyleSheet.create({
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
   container: { flex: 1, alignItems: "center" },
   searchBarPos: {
     justifyContent: "flex-start",
     marginTop: 20,
+    flex: 1,
   },
   title: {
     flex: 0.5,
