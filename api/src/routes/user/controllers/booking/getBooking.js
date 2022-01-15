4a443e2590b2beeaf1aff92f3734d518fa77c2a2
Leo Wirth <leo_wirth@hotmail.com>
2022-01-15 20:37:27 -0300

    Se modifica modelo de Bookings, y el getBooking, ahora trae mas data y solo de las reservas que se encuentren activas

--- api/src/routes/user/controllers/booking/getBooking.js
+++ api/src/routes/user/controllers/booking/getBooking.js
@@ -1,16 +1,26 @@
-const { Bookings } = require("../../../../db");
+const { Bookings, Field } = require("../../../../db");
 
 const getBooking = async (req, res) => {
   const { userId } = req.params;
 
   const booking = await Bookings.findAll({
-    where: { userId: `${userId}` },
+    where: { userId: `${userId}`, status: "active" },
   }).catch((err) => console.log(err));
-  // console.log("nooking: ", booking);
   if (!booking.length) {
     return res.json({ message: "Datos incorrectos" });
   } else {
-    res.json({ booking: booking });
+    var result = [];
+    for (let i = 0; i < booking.length; i++) {
+      result[i] = {
+        booking: booking[i],
+        court: await Field.findOne({
+          where: {
+            id: booking[i].courtId,
+          },
+        }),
+      };
+    }
+    res.json({ result });
   }
 };
 
