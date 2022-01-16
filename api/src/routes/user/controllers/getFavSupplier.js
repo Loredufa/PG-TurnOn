fda5426d8d36be79cbec43dbd6123af37ed8a305
unknown <lubelleggia@gmail.com>
2022-01-16 12:19:05 -0300

    se modifica controlador getCourts para front mobile

--- api/src/routes/user/controllers/getFavSupplier.js
+++ api/src/routes/user/controllers/getFavSupplier.js
@@ -24,23 +24,3 @@ const getFavSupplier = async (req, res) => {
 module.exports = {getFavSupplier}
 
 
-// let { userId } = req.params
-           
-    // let courtsfavs
-
-    // try {
-
-    //     if (userId) {
-    //         courtsfavs = await Favorites.findAll({ where: { userId } })
-    //     }
-
-    //     else {
-    //         courtsfavs = await Field.findAll({})
-    //     }
-    // }
-    // catch (error) {
-    //     throw new Error("Error al encontrar a las canchas favoritas solicitada")
-    // }
-
-    // res.send(courtsfavs)
-
