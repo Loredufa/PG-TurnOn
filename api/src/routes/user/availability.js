c9d80e4ca3b23423ca582273fcfd9eb5875cf86d
Lorena <88742175+Loredufa@users.noreply.github.com>
2022-01-16 12:27:36 -0300

    falta comprobacion

--- api/src/routes/user/availability.js
+++ api/src/routes/user/availability.js
@@ -1,6 +1,9 @@
 const {Router} = require ("express")
 const router = Router () 
 
+const { getAvailability } = require("./availability/getAvailability");
+
+router.get("/available/:idCourt", getAvailability);
 
 // router.get ("/" , (req, res) => {
 //     res.send ("Soy la ruta usuario/disponibilidad")
