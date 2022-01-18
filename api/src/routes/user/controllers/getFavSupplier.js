ebb0c0ca663e778fec68d2d7abe8a5ed8dce8d00
unknown <lubelleggia@gmail.com>
2022-01-18 12:16:11 -0300

    filtro en favoritos

--- api/src/routes/user/controllers/getFavSupplier.js
+++ api/src/routes/user/controllers/getFavSupplier.js
@@ -5,20 +5,29 @@ const {User} = require("../../../db")
 const getFavSupplier = async (req, res) => {
 
     const {userId} = req.body
+    const {name} = req.query
+    
 
-      
     try {
       
          const user = await User.findByPk(userId)
-         const supplier_fav = await user.getSuppliers()
-         res.json (supplier_fav)
+         let supplier_fav = await user.getSuppliers()
+         
 
-        
+         if (name) {
+            supplier_fav = supplier_fav
+            .filter((e)=> e.name.toLowerCase()
+            .includes(name.toLowerCase()))
+                    
+        }
+
+        res.json (supplier_fav)
      } catch (error) {
+
          console.log(error)
          throw new Error("Error al encontrar los favoritos");
-       }
      
+        }
 }
 
 module.exports = {getFavSupplier}
