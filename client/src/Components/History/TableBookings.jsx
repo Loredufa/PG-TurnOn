093571cf89bf754795be0b0739ddadd4553086c4
Orhianna <orhianna.ss@gmail.com>
2022-01-22 17:26:13 -0300

    Se Cambio el componente de historial que muestra el booking

--- client/src/Components/History/TableBookings.jsx
+++ client/src/Components/History/TableBookings.jsx
@@ -1,19 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector } from "react-redux";
-import MaterialTable from 'material-table';
-/* import { makeStyles, ThemeProvider } from '@material-ui/core'; */
-/* import { Modal, TextField, Button } from '@material-ui/core'; */
-import { makeStyles } from '@material-ui/core/styles';
+import MaterialTable from '@material-table/core';
 import axios from 'axios';
 
-const useStyles = makeStyles ((theme) => ({
+
+/* const useStyles = makeStyles ((theme) => ({
     modal:{
         position: "absolute",
         width: 400,
         backgroundColor: theme.palette.background.paper,
         border: '2px solid #000',
         boxShador: theme.shadows[5],
-        padding: theme.spacing(2,4,3),
+        padding: theme.spacing(2, 4, 3),
         top: '50%',
         left: '50%',
         transform: 'translate(-50%, -50%)'
@@ -25,15 +23,17 @@ const useStyles = makeStyles ((theme) => ({
         width: '100%'
     }
 
-}))
+})) */
 
 const columns =[
-    { title:"Numero", field:"id", type: "numeric"},
+    { title:"Id", field:"id", type: "numeric"},
     { title:"Fecha", field:"date"},
-    { title:"Id Cancha", field:"courtId"},
+    { title:"Día", field:"day"},
+    { title:"Cancha", field:"courtId"},
     { title:"Hora de inicio", field:"initialTime"},
     { title:"Hora de finalización", field:"endingTime"},
-    { title:"Nombre y Apellido", field: "user"},
+    { title:"Nombre", field: "user.name"},
+    { title:"Apellido", field: "user.lastname"},
     { title:"Teléfono", field: "user.phone"},
     { title:"Estado", field:"status"},
     { title:"Codigo de Reserva", field:"bookingCode"}
@@ -41,37 +41,30 @@ const columns =[
 
 
 export default function TableBookings (){
-    const styles = useStyles();
-    const supplierId = useSelector((e) => e.user.supplier.id);
+    const {supplier} = useSelector(state => state.user);
     const [info, setInfo] = useState([]);
 
-    const getBokking = async ()=>{
-        await axios.get(`/supplier/bookings?supplierId=${supplierId}`)
-        .then(response => {setInfo(response.data)})
-        .catch (error => { console.log(error)})
-    }
+    useEffect(() => {
+        axios.get(`/supplier/bookings?supplierId=${supplier.id}`)
+            .then(res => setInfo(res.data))
+            .catch(error => {console.log(error)})
+            /* console.log("QUE TRAE", info) */
+            console.log("QUE Suplier.id", supplier.id)
+    }, [supplier.id])
 
-    useEffect (() => {
-        getBokking()
-    }, [])
-   
-    
+
  
     return(
         <div>
+            <br/>
             <MaterialTable
                 columns={columns}
-                data={info}
+                data={info.allbookings}
                 title="Historial de Reservas"
-                actions={[
-                    { icon: "edit", tooltip: "Editar Estado"},
-                    { icon: "delete", tooltip: "Eliminar reserva"}
-                ]}
                 options={{ actionsColumnIndex: -1}}
                 localization={{ header:{ actions:"Acciones"}}}
             
             />
-
         </div>
     )
 }
