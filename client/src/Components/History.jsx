0d8e065f3f7b8030ff2492361225da64a4af7ef8
Leo Wirth <leo_wirth@hotmail.com>
2022-01-25 22:30:33 -0300

    Se acomoda el padding de la navbar

--- client/src/Components/History.jsx
+++ client/src/Components/History.jsx
@@ -1,18 +1,32 @@
-import React from 'react'
-import TableBookings from './History/TableBookings'
-import styled from "styled-components"
-
+import React from "react";
+import TableBookings from "./History/TableBookings";
+import styled from "styled-components";
 
 export default function History() {
-    return (
-        <div>
-            <Title>Historial de Reservas</Title>
-            <TableBookings/>
-        </div>
-    )
+  return (
+    <BackGround>
+      <Title>Historial de Reservas</Title>
+
+      <TableBookings />
+    </BackGround>
+  );
 }
 
 export const Title = styled.h1`
-color: #81b214;
-margin-left: 2%;
-font-size: 300%`
+  color: #81b214;
+  margin-left: 2%;
+  font-size: 300%;
+`;
+
+export const BackGround = styled.div`
+  background: #e9ebed;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  width: 100vw;
+  height: 90.5vh;
+  background-attachment: fixed;
+  overflow: visible;
+  overflow-x: hidden;
+  padding-top: 80px;
+`;
